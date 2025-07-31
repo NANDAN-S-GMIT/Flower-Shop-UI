2fa47922fa9de4f639ad3fa17c8811d82453f256
Mr.Nandan.S <benz.nandan.s@gmail.com>
2025-07-31 23:20:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Flower-Shop-UI/',   // MUST match GitHub repo name exactly
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Flower-Shop/' : '/',
-});
+})
